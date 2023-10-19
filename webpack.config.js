cc4c4a3362d9eb244a288730e4aaefef4a0a9845
Simona Pizio <simona.pizio@gmail.com>
2023-10-19 12:15:12 +0200

    Install and update dependencies - update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 'use strict';
 
 const webpack = require('webpack'); // eslint-disable-line no-unused-vars
+const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './browser/index.js',
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, 'dist'),
     filename: './public/js/bundle.js',
   },
   context: __dirname,
   devtool: 'source-map',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2'],
-        }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-react', { targets: "defaults" }]
+            ]
+          }
+        },
       }
     ]
   }
