08ee216b6bbb4782228609a42a088dd4fb9d9fe4
Simona Pizio <simona.pizio@gmail.com>
2023-10-20 11:03:41 +0200

    #3 API routes - add GET minions

--- server/minionsRouter.js
+++ server/minionsRouter.js
@@ -4,4 +4,13 @@ const minionsRouter = express.Router({mergeParams: true});
 // route /api/minions
 module.exports = minionsRouter;
 
-
+// GET /api/minions to get an array of all minions.
+minionsRouter.get('/', (req, res, next) => {
+  // to implement
+  const minions = [];
+  if (minions) {
+    res.status(200).send(minions);
+  } else {
+    res.status(404).send();
+  }
+});
