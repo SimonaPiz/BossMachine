288241ba61d3bba28645b0adb1ca35b53725fbda
Simona Pizio <simona.pizio@gmail.com>
2023-10-20 11:34:47 +0200

    #3 API routes - add GET all meetings

--- server/meetingsRouter.js
+++ server/meetingsRouter.js
@@ -3,3 +3,14 @@ const express = require('express');
 const meetingsRouter = express.Router({mergeParams: true});
 
 module.exports = meetingsRouter;
+
+// GET /api/meetings to get an array of all meetings.
+meetingsRouter.get('/', (req, res, next) => {
+  // to implement
+  const meetings = [];
+  if (meetings) {
+    res.status(200).send(meetings);
+  } else {
+    res.status(404).send();
+  }
+});
