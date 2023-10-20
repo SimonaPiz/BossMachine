1c8346144d0759d4a9741c63141696c6089fbb7a
Simona Pizio <simona.pizio@gmail.com>
2023-10-20 17:19:04 +0200

    #8 Bonus - add GET in work router

--- server/workRouter.js
+++ server/workRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // import helper functions
 const { } = require('./db');
+const { getAllWorksByMinionsId } = require('../../project-4-boss-machine-start/project-4-boss-machine-start/server/db');
 
 /*Bonus: you may implement routes to allow bosses 
 to add and remove work from their minions' backlogs.
@@ -16,3 +17,13 @@ Schema:
 const workRouter = express.Router({mergeParams: true});
 
 module.exports = workRouter;
+
+// GET /api/minions/:minionId/work to get an array of all work for the specified minon.
+workRouter.get('/', (req, res, next) => {
+  const works = getAllWorksByMinionsId(req.minionId);
+  if (works) { 
+    res.status(200).send(works);
+  } else {
+    res.status(404).send();
+  }
+});
