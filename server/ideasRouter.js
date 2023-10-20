32c5bd46098b50b17fb7e4833d7f7beafde8444f
Simona Pizio <simona.pizio@gmail.com>
2023-10-20 11:23:45 +0200

    #3 API routes - add GET all ideas

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const ideasRouter = express.Router({mergeParams: true});
 
+// router for /api/ideas
 module.exports = ideasRouter;
+
+// GET /api/ideas to get an array of all ideas.
+ideasRouter.get('/', (req, res, next) => {
+  // to implement
+  const ideas = [];
+  if (ideas) {
+    res.status(200).send(ideas);
+  } else {
+    res.status(404).send();
+  }
+});
